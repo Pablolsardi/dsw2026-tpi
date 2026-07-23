566d41582f6c066d6501bae2ff9c252731b6ac24
Diego Luna <Diego.LunaMendoza@alu.frt.utn.edu.ar>
2026-07-22 23:03:59 -0300

    feat: configuraciones EF con índices únicos, conversiones de enums y query filters

---
+++ Dsw2026Tpi.Data/Configurations/AppointmentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations;
+
+public class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+{
+    public void Configure(EntityTypeBuilder<Appointment> builder)
+    {
+        builder.ToTable("Appointments");
+
+        builder.Property(a => a.Reason).IsRequired().HasMaxLength(300);
+        builder.Property(a => a.Status).HasConversion<string>().HasMaxLength(20);
+
+        // Token de concurrencia: SQL Server actualiza esta columna en cada UPDATE
+        builder.Property(a => a.RowVersion).IsRowVersion();
+
+        // Un slot solo puede tener una cita
+        builder.HasIndex(a => a.AvailabilitySlotId).IsUnique();
+
+        builder.HasQueryFilter(a => !a.Deleted);
+    }
+}
--- Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
+++ Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
@@ -9,5 +9,11 @@ public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
     public void Configure(EntityTypeBuilder<Doctor> builder)
     {
         builder.ToTable("Doctors");
+
+        builder.Property(d => d.Name).IsRequired().HasMaxLength(100);
+        builder.Property(d => d.LicenseNumber).HasMaxLength(50);
+
+        builder.HasQueryFilter(d => !d.Deleted);
+
     }
 }
