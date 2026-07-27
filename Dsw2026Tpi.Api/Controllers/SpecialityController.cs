f21717e5444862a2b981c5df9f7553fcf3fb8ca3
lopezsardip <lopezsardip@gmail.com>
2026-07-27 17:17:41 -0300

    crear endpoints y actualizar DependencyInjectionConfigurationExtensions.cs

---
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -0,0 +1,52 @@
+﻿using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Dsw2026Tpi.Application.Dtos;
+
+namespace Dsw2026Tpi.Api.Controllers;
+
+[Route("api/specialties")]
+[Authorize(Policy = Policies.AdminPolicy)]
+public class SpecialityController : AppController
+{
+    private readonly ISpecialityService _service;
+        
+    public SpecialityController(ISpecialityService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(
+       [FromQuery] int pageSize = 10,
+       [FromQuery] int pageIndex = 1,
+       [FromQuery] string? name = null)
+    {
+        var result = await _service.GetAll(pageSize, pageIndex, name);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] SpecialityModel.Request request)
+    {
+        var result = await _service.Create(request);
+        return StatusCode(StatusCodes.Status201Created, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] SpecialityModel.Request request)
+    {
+        var result = await _service.Update(id, request);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await _service.Delete(id);
+        return NoContent();
+    }
+}
+
