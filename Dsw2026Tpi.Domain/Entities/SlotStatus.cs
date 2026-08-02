ce7985f9f77e9dd82b4ae000c5c32d1e2613d28b
Diego Luna <Diego.LunaMendoza@alu.frt.utn.edu.ar>
2026-08-02 16:58:38 -0300

    Terminar contrato inicial de availabilities

--- Dsw2026Tpi.Domain/Entities/SlotStatus.cs
+++ Dsw2026Tpi.Domain/Entities/SlotStatus.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 namespace Dsw2026Tpi.Domain.Entities
 {
-    // Estado del slot de disponibilidad (tabla AVAILABILITYSLOTS)
     public enum SlotStatus
     {
         Available,
