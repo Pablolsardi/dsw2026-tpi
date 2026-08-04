1057ec346395415d0f3d09e675c316b47c07fce5
Fabri519 <fabrizzio.lopezroldan@alu.frt.utn.edu.ar>
2026-08-04 11:51:46 -0300

    update IappointmentService

--- Dsw2026Tpi.Application/Interfaces/IAppointmentService.cs
+++ Dsw2026Tpi.Application/Interfaces/IAppointmentService.cs
@@ -1,4 +1,5 @@
 ﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Domain.Entities;
 
 namespace Dsw2026Tpi.Application.Interfaces;
 
@@ -7,4 +8,7 @@ public interface IAppointmentService
     Task<AppointmentModel.Response> Create(AppointmentModel.Request request);
     Task<IEnumerable<AppointmentModel.Response>> GetByPatientDni(long dni);
     Task Cancel(Guid id);
+    Task<Pagination<AppointmentModel.AdminResponse>> GetByDate(DateOnly? date, int pageSize, int pageIndex);
+    Task<Pagination<AppointmentModel.AdminResponse>> Search(
+        Guid? specialtyId, Guid? doctorId, long? dni, DateOnly? date, int pageSize, int pageIndex);
 }
